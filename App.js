07d937ec8d998f3d2d04c1a5e26ba0c5bfa30a77
Shruti WebGit <27shrutisharma.06@gmail.com>
2023-12-26 19:34:12 +0530

    Node files uploaded

---
+++ App.js
@@ -0,0 +1,4 @@
+const { addition,multiply } = require("./calculation")
+console.log("This is Node file first project of node")
+console.log(addition(60,40))
+console.log(multiply(10,50))
---
+++ calculation.js
@@ -0,0 +1,10 @@
+// function addition(a,b){
+//     return a+b
+// }
+// function multiply(a,b){
+//     return a*b
+// }
+// module.exports={addition,multiply}
+// module.exports=multiply
+exports.addition=(a,b)=>a+b
+exports.multiply=(a,b)=>a*b
