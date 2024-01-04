39ec9e070b5389f28cf4561cf6b7d031a751bfd5
Shruti WebGit <27shrutisharma.06@gmail.com>
2024-01-04 19:16:04 +0530

    New methods (Post,Patch,Delete)

--- express-server.js
+++ express-server.js
@@ -1,10 +1,17 @@
 // Install express first npm i express
 
+const fs=require('fs')                 //require fs file to use fs 
+
 
 const users=require('./MOCK_DATA.json')
 const http=require ('http')
 const express=require ('express')
 const app=express();
+//app.use(express.urlencoded())            
+app.use(express.json())
+
+// ***********************************************
+// GET Method  (new method (02/01/2024))
 app.get('/api/users',(req,res)=>{          //get is a method
     return res.json(users)
 })
@@ -17,7 +24,60 @@ app.get('/api/users/:id',(req,res)=>{
     const id=parseInt(req.params.id)
     const user=users.find((user)=> user.id === id)        //=== is used to check value with data type
     return res.json(user)
-})
+})       // ************** END
+
+
+// ***********************************************
+// POST Method  (new method (04/01/2024))
+app.post("/api/users", (req, res) => {
+    const body = req.body
+    users.push({ ...body, id: users.length + 1 })
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status: "success" })
+    });
+});      // ********** END
+
+// ********************************************
+//PATCH Method   (new method (04/01/24024))
+app.patch("/api/users/:id",(req,res)=>{
+    //const id=req.params.id;
+    const id=parseInt(req.params.id)
+    const user=users.find(u=>u.id===id)
+    if(!user){
+        return res.status(404).json({
+            status:"Fail",
+            message:"User not found"
+        });
+    }
+    const i=users.indexOf(user)
+    Object.assign(user,req.body)
+    users[i]=user
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status: "User updated successfully" })
+    });
+})    // ************* END
+
+
+// ********************************************
+//DELETE Method   (new method (04/01/24024))
+app.delete("/api/users/:id",(req,res)=>{
+    //const id=req.params.id;
+    const id=parseInt(req.params.id)
+    const user=users.find(u=>u.id===id)
+    if(!user){
+        return res.status(404).json({
+            status:"Fail",
+            message:"User not found"
+        });
+    }
+    const i=users.indexOf(user)
+    users.splice(i,1)   //1 is used to delete one data (If we have to delete more data inc. value)
+   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ status: "Success", message: "User deleted successfully" })
+    });
+})    // ********** END
+
+
 app.get('/about',(req,res)=>{
     return res.end("<h1>About page</h1>")
 })
